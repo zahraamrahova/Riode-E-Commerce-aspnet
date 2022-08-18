71b4b51db17dd181981bee56e9f7dd32c79bcf6d
zahraamrahova <zehra.khudaverdiyeva@gmail.com>
2022-08-18 15:59:44 +0300

    Merge main with feature lesson

---
+++ Riode.WebUI/Riode.WebUI/Models/Entities/Brand.cs
@@ -0,0 +1,13 @@
+﻿namespace Riode.WebUI.Models.Entities
+{
+    public class Brand
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int? CreatedByUserId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int? DeletedByUserId { get; set; }
+        public DateTime? DeletedDate { get; set; }
+    }
+}
